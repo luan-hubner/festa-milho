a60631c7decfc78872187d5906d9b7ae91d576d9
Luan Hubner <luanhubner.pro@gmail.com>
2022-04-16 16:53:11 -0400

    feat: initial app config

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+
+import Home from './src/screens/home'
+
+import { useFonts, Poppins_700Bold_Italic } from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Poppins_700Bold_Italic
+  })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
+  return <Home />
+}
