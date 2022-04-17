99f821e4f9a347ed5f72fa590fbc9b113bcb85b3
Luan Hubner <luanhubner.pro@gmail.com>
2022-04-17 16:27:46 -0400

    feat: add pages to router

--- App.tsx
+++ App.tsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import * as React from 'react'
 
 import Home from './src/screens/home'
 
-import { useFonts, Poppins_700Bold_Italic } from '@expo-google-fonts/poppins'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import {
+  useFonts,
+  Poppins_700Bold_Italic,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 import AppLoading from 'expo-app-loading'
+import FoodMenu from './src/screens/food-menu'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Poppins_700Bold_Italic
+    Poppins_700Bold_Italic,
+    Poppins_700Bold
   })
 
   if (!fontsLoaded) {
     return <AppLoading />
   }
 
-  return <Home />
+  const Stack = createNativeStackNavigator()
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{
+            headerShown: false
+          }}
+          component={Home}
+        />
+        <Stack.Screen
+          name="Cardápio"
+          options={{
+            headerShown: false
+          }}
+          component={FoodMenu}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
